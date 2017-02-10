17f30fdcd8807c6bb801c791dcd15202986423cf
Qianlu Shen <Qianlu Shen>
2017-02-10 14:29:24 +0800

    完成get/post的提交返回

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const logger = require('koa-logger');
 const koa = require('koa');
 const app = koa();
 const KeyGrip = require('keygrip');
-
+const router = require('./config/routers');
+const views = require('koa-views');
 // app.keys = "hahah"
 
 // 中间件 x-response-time
 
-app.use(function *(next){
+app.use(function* (next) {
   var start = new Date;
   yield next;
   var ms = new Date - start;
@@ -21,10 +22,27 @@ app.use(logger());
 // app.keys = "asda"
 // app.keys = new KeyGrip(['im a newer secret', 'i like turtle'], 'sha256');
 
-const router = require('./config/routers');
+
+
+
+
+
+
 
 app.use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
+  .use(views(__dirname + '/views', {
+    map: {
+      html: 'nunjucks'
+    }
+  }))
+  .use(function* () {
+    this.state = {
+      session: this.session,
+      title: 'app'
+    };
+    yield this.render(this.url, this.data);
+  });
 
 /* app.use(function *(){
   // this.cookies.set('name', 'tobi', {signed: true})
@@ -34,7 +52,8 @@ app.use(router.routes())
 
 // app.listen(3000);
 
-app.on('error', function(err){
+
+app.on('error', function (err) {
   console.error('server error', err);
 });
 
--- controller/users.js
+++ controller/users.js
@@ -1,4 +1,6 @@
 const users = require('koa-router')();
+
+
 users.get('/:id', function* (next) {
   this.log = yield function () {
     return { id: 3 } //做登录验证
@@ -6,7 +8,30 @@ users.get('/:id', function* (next) {
   yield next
 },
   function* (next) {
-    this.body = this.log
+    if(parseInt(this.params.id) > 3){
+      this.url = "users";
+      this.data = {user:"SHenqi"}
+    } else {
+      this.url = "users2";
+      this.data = {user:"SHenqiasdasd"}
+    }
+    
+    yield next
+   console.log(123123123)
+  }
+);
+
+users.post('/node', function* (next) {
+  this.log = yield function () {
+    return { id: 3 } //做登录验证
+  }()
+  yield next
+},
+  function* (next) { 
+  this.body = {
+    code: 200,
+    msg: "有arefool"
+  }
   }
 );
 
